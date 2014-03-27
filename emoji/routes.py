44d8c86290cfadce501cc865ff922b4dff3d77a1
Kate Dubuisson <kate.dubuisson@gmail.com>
2014-03-26 22:54:17 -0600

    deleted helpers and cleaned up routes

--- emoji/routes.py
+++ emoji/routes.py
@@ -1,34 +1,41 @@
 from flask import render_template, request, session, url_for, redirect
 
+import random
+
 from emoji import app
 from models import Emoji
 from models import db
-import helpers
 
-@app.route('/emoji/', methods=['GET', 'POST'])
-def get_emoji():
-    if request.method == 'POST':
-        emoji = Emoji(request.form.get('text'), int(request.form.get('category_id')))
-        db.session.add(emoji)
-        db.session.commit()
+@app.route('/emoji/create/', methods=['POST'])
+def create_emoji():
+    emoji = Emoji(request.form.get('text'), int(request.form.get('category_id')))
+    db.session.add(emoji)
+    db.session.commit()
 
-        return emoji.text
+    return emoji.text
+
+@app.route('/emoji/', methods=['GET'])
+def random_emoji():
+    all_emoji = Emoji.query.all()
+    if len(all_emoji):
+        return random.sample(all_emoji, 1)[0].text
     else:
-        # return a random emoji!!
-        pass
+        return ''
 
 @app.route('/emoji/<int:emoji_id>', methods=['GET', 'PUT', 'DELETE'])
 def get_emoji_by_id(emoji_id):
-    emoji_id = helpers.ensure_int(emoji_id)
-    emoji = Emoji.query.get(emoji_id)
+    text = request.args.get('text', None)
+
+    emoji = Emoji.query.get_or_404(emoji_id)
 
     if request.method == 'PUT':
+        if not text:
+            return 'Text is required'
         # update emoji!!
         # db.session.commit()
-
-        return repr(emoji)
+        return emoji.text
     elif request.method == 'DELETE':
         db.session.delete(emoji)
         db.session.commit()
     else:
-        return repr(emoji)
+        return emoji.text
